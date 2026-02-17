7e47da42ba7ae909b1f64f9c279e6a5baade72a1
EvelyParanhos <evelyparanhos05@gmail.com>
2026-02-17 17:11:14 -0300

    feat: adding files with framework Spring Boot

---
+++ financas/src/main/java/com/evely/financas/FinancasApplication.java
@@ -0,0 +1,13 @@
+package com.evely.financas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancasApplication.class, args);
+	}
+
+}
