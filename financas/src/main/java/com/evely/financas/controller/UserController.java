9b3332a3257cc39e28def6afffb368904792a497
EvelyParanhos <evelyparanhos05@gmail.com>
2026-02-21 16:59:36 -0300

    feat: CRUD user

---
+++ financas/src/main/java/com/evely/financas/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.evely.financas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.evely.financas.model.User;
+import com.evely.financas.service.UserService;
+import lombok.RequiredArgsConstructor;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity <User> salvar(@RequestBody User user) {
+        return ResponseEntity.status(201).body(userService.salvar(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> listarTodos () {
+        return ResponseEntity.ok(userService.listarTodos());
+    }
+
+    @DeleteMapping ("/{id}")
+    public ResponseEntity<Void> excluir (@PathVariable Integer id) {
+        userService.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity <User> editar(@PathVariable Integer id, @RequestBody User user) {
+        return ResponseEntity.ok(userService.editar(id, user));
+    }
+}
---
+++ financas/src/main/java/com/evely/financas/service/UserService.java
@@ -0,0 +1,35 @@
+package com.evely.financas.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.evely.financas.model.User;
+import com.evely.financas.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User salvar (User user) {
+        User usuarioSalvo = userRepository.save(user);
+        return userRepository.findById(usuarioSalvo.getId()).get();    
+    }
+
+    public List<User> listarTodos() {
+        return userRepository.findAll();
+    }
+
+    public void excluir (Integer id) {
+        userRepository.deleteById(id);
+    }
+
+    public User editar (Integer id, User usuarioAtualizado) {
+        User usuarioExistente = userRepository.findById(id)
+            .orElseThrow(()-> new RuntimeException("Usuário não encontrado!"));
+        usuarioExistente.setName(usuarioAtualizado.getName());
+        usuarioExistente.setTelegramId(usuarioAtualizado.getTelegramId());
+        return userRepository.save(usuarioExistente);
+    }
+    
+}
