4a6123d3db04d531e49e285b8e7b0827016f4398
EvelyParanhos <evelyparanhos05@gmail.com>
2026-02-26 21:52:41 -0300

    feat: adding column creat_at at Transaction to implement logic of simulation

--- financas/src/main/java/com/evely/financas/repository/InstallmentRepository.java
+++ financas/src/main/java/com/evely/financas/repository/InstallmentRepository.java
@@ -11,13 +11,13 @@ import com.evely.financas.model.Installment;
 
 @Repository
 public interface InstallmentRepository extends JpaRepository <Installment, Integer> {
-    @Query("SELECT SUM(i.amount) FROM Installment i WHERE i.payer.id = :userId AND i.status = :status")
-    BigDecimal somarTotalPorUsuarioEStatus(@Param("userId") Integer userId, @Param("status") InstallmentStatus status);
+       @Query("SELECT SUM(i.amount) FROM Installment i WHERE i.payer.id = :userId AND i.status = :status")
+       BigDecimal somarTotalPorUsuarioEStatus(@Param("userId") Integer userId, @Param("status") InstallmentStatus status);
 
-    @Query("SELECT SUM(i.amount) FROM Installment i WHERE i.payer.id = :userId " +
-           "AND i.status = :status AND i.dueDate BETWEEN :inicio AND :fim")
-    BigDecimal somarMensalPorUsuario(@Param("userId") Integer userId, 
-                                     @Param("status") InstallmentStatus status, 
-                                     @Param("inicio") LocalDate inicio, 
-                                     @Param("fim") LocalDate fim);
+       @Query("SELECT SUM(i.amount) FROM Installment i WHERE i.payer.id = :userId " + "AND i.status = :status AND i.dueDate BETWEEN :inicio AND :fim")
+       BigDecimal somarMensalPorUsuario(@Param("userId") Integer userId, @Param("status") InstallmentStatus status, @Param("inicio") LocalDate inicio, @Param("fim") LocalDate fim);
+
+       @Query("SELECT SUM(i.amount) FROM Installment i " + "JOIN i.transaction t " + "WHERE i.payer.id = :userId " + "AND i.status = 'PENDING' " + "AND i.dueDate BETWEEN :inicio AND :fim " +"AND (:incluirSimulacoes = true OR t.isSimulation = false)")
+       BigDecimal somarDividasComFiltro(@Param("userId") Integer userId, @Param("inicio") LocalDate inicio, @Param("fim") LocalDate fim, @Param("incluirSimulacoes") boolean incluirSimulacoes);
 }
+
