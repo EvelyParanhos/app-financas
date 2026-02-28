16460c9a8fc628aa79b5c88a9f8f8c2671cc9fbd
EvelyParanhos <evelyparanhos05@gmail.com>
2026-02-28 11:30:38 -0300

    feat: when an account is credit card, an initial snapshot is registred

--- financas/src/main/java/com/evely/financas/service/AccountService.java
+++ financas/src/main/java/com/evely/financas/service/AccountService.java
@@ -1,27 +1,44 @@
 package com.evely.financas.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.stereotype.Service;
+import com.evely.financas.enums.AccountType;
 import com.evely.financas.model.Account;
+import com.evely.financas.model.Snapshot;
 import com.evely.financas.repository.AccountRepository;
+import com.evely.financas.repository.SnapshotRepository;
 import com.evely.financas.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
+
     private final AccountRepository accountRepository;
     private final UserRepository userRepository;
+    private final SnapshotRepository snapshotRepository;
 
+    @Transactional
     public Account salvar (Account account) {
         Integer ownerId = account.getOwner().getId();
 
         userRepository.findById(ownerId)
             .orElseThrow(()->new RuntimeException("Não foi possível criar a conta: Usuário dono não encontrado!"));
         
+        if (account.getType() == AccountType.CREDIT_CARD) {
+            Snapshot initialSnapshot = new Snapshot();
+            initialSnapshot.setAccount(account);
+            initialSnapshot.setAmount(account.getCardLimit());
+            initialSnapshot.setSnapshotDate(LocalDateTime.now());
+            snapshotRepository.save(initialSnapshot);
+        }
+    
         Account salva = accountRepository.save(account);
         return accountRepository.findById(salva.getId()).get();
     }
+
 
     public List<Account> listarTodas() {
         return accountRepository.findAll();
