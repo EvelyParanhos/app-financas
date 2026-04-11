b22a522c3fb04f4ef65d52309f05fe6e4ec195e2
Evely Paranhos <evelyparanhos05@gmail.com>
2026-04-11 17:42:20 -0300

    fix: fixing type of ID at PartnershipRepository

--- financas/src/main/java/com/evely/financas/repository/PartnershipRepository.java
+++ financas/src/main/java/com/evely/financas/repository/PartnershipRepository.java
@@ -1,13 +1,14 @@
 package com.evely.financas.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.evely.financas.model.Partnership;
 
 @Repository
-public interface PartnershipRepository extends JpaRepository <Partnership, Integer> {
+public interface PartnershipRepository extends JpaRepository <Partnership, UUID> {
     @Query("SELECT p FROM Partnership p WHERE p.userA.id = :userId OR p.userB.id = :userId")
-    Optional<Partnership> findByUserId(Integer userId);
+    Optional<Partnership> findByUserId(UUID userId);
 }
